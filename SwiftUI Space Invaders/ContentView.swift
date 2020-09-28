2a223685c71f7a200fb7c87a6f16c304bab915dd
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-09-28 16:34:40 -0700

    SwiftUI Experimentation

--- SwiftUI Space Invaders/ContentView.swift
+++ SwiftUI Space Invaders/ContentView.swift
@@ -7,12 +7,120 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ShipView:View {
+    var currentLocation = CGSize()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image(systemName: "circle")
+            .resizable()
+            .frame(width:40, height:40)
+            .offset(currentLocation)
+            .animation(.easeOut(duration: 0.3))
     }
 }
+struct MissileView:View {
+    var currentLocation = CGSize()
+    var fireLocation = CGSize()
+    var body: some View {
+        Image(systemName: "chevron.compact.up")
+            .resizable()
+            .frame(width: 10, height: 10)
+            .opacity(1)
+            .offset(currentLocation)
+            .animation(.easeIn(duration: 0.3))
+    }
+}
+
+struct ContentView:View {
+    @State private var offSet = CGSize(width: 0, height: 0)
+    @State private var characterLocation = CGSize(width: 0, height: 0)
+    @State private var missileLocation = CGSize(width: 0, height: 0)
+    var body: some View {
+        VStack{
+            HStack{
+                Text("\(self.characterLocation.debugDescription)")
+                Text("RESET").foregroundColor(.red).onTapGesture {
+                    self.offSet.width = 0
+                    self.offSet.height = 0
+                    self.characterLocation = self.offSet
+                    self.missileLocation = self.offSet
+                }
+            }
+            Spacer()
+            ZStack {
+                
+                ShipView(currentLocation: offSet)
+                
+                MissileView(currentLocation: offSet)
+                
+            }
+            
+            //MARK:- Button Controls
+            VStack {
+                Button("UP") {
+                    self.upButtonPressed()
+                    
+                }
+                HStack {
+                    Spacer()
+                    Button("LEFT") {
+                        self.leftButtonPressed()
+                    }
+                    Spacer()
+                    Button("RIGHT") {
+                        self.rightButtonPressed()
+                    }
+                    .offset(x: 40.0, y: 0)
+                    Spacer()
+                    Button("FIRE") {
+                        self.fireButtonPressed()
+                        self.missileLocation.height -= 1000
+                    }
+                    .offset(x: 30.0, y: 0)
+                    .foregroundColor(.red)
+                }
+                Button("DOWN"){
+                    self.downButtonPressed()
+                }
+            }.offset(x: -60.0, y: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+        }
+    }
+    
+    /*
+     Mark:- Each Button Method when called modifies the "offSet" State. When doing so it refreshes our view.
+     */
+    func fireButtonPressed(){
+        missileLocation.height -= 1000
+        
+    }
+    func downButtonPressed(){
+        offSet.height += 100
+        characterLocation = offSet
+        missileLocation = characterLocation
+        
+    }
+    
+    func upButtonPressed(){
+        offSet.height -= 100
+        characterLocation = offSet
+        missileLocation = characterLocation
+    }
+    
+    func rightButtonPressed(){
+        offSet.width += 100
+        characterLocation = offSet
+        missileLocation = offSet
+    }
+    
+    func leftButtonPressed(){
+        offSet.width -= 100
+        characterLocation = offSet
+        missileLocation = characterLocation
+    }
+    
+    
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
