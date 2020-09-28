e953d1bbaafd28a94115af89505770c3ec16ef10
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-09-28 11:00:22 -0700

    Initial Commit

---
+++ SwiftUI Space Invaders/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Space Invaders
+//
+//  Created by Alphonso Sensley II on 9/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Space Invaders/SwiftUI_Space_InvadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Space_InvadersApp.swift
+//  SwiftUI Space Invaders
+//
+//  Created by Alphonso Sensley II on 9/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Space_InvadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
